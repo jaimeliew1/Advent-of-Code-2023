df27d25d7795efd478e687f24f7ab2852dcb284c
Jaime Liew <33415790+jaimeliew1@users.noreply.github.com>
2023-12-01 09:56:27 -0500

    Update day01.py

--- advent_of_code_2023/day01.py
+++ advent_of_code_2023/day01.py
@@ -4,17 +4,7 @@ from typing import List, Dict, Tuple
 INPUT_FN = Path(__file__).parent.parent / "input/day01.txt"
 
 numbers = {"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9}
-numbers_2 = {
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
-}
+numbers_2 = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
 
 
 def parse(fn: Path) -> List[str]:
@@ -22,7 +12,7 @@ def parse(fn: Path) -> List[str]:
         return f.readlines()
 
 
-def firstoccurence(of, string: str) -> str:
+def first_occurence(of: List[str], string: str) -> str:
     while len(string) > 0:
         for tofind in of:
             if string.startswith(tofind):
@@ -33,8 +23,8 @@ def firstoccurence(of, string: str) -> str:
 
 
 def calibration_value(string: str, numbers: Dict[str, int]) -> int:
-    first = firstoccurence(numbers.keys(), string)
-    last = firstoccurence([x[::-1] for x in numbers.keys()], string[::-1])[::-1]
+    first = first_occurence(numbers.keys(), string)
+    last = first_occurence([x[::-1] for x in numbers.keys()], string[::-1])[::-1]
     return 10 * numbers[first] + numbers[last]
 
 
