684c570cc5fcee2fb944c92028b327199cd76808
Jaime Liew <jaimeliew1@gmail.com>
2023-12-04 13:31:04 -0500

    add day04 part 2

--- advent_of_code_2023/day04.py
+++ advent_of_code_2023/day04.py
@@ -3,22 +3,26 @@ from pathlib import Path
 INPUT_FN = Path(__file__).parent.parent / "input/day04.txt"
 
 
-def parse(fn: Path) -> list[tuple[list[int], list[int]]]:
+def parse(fn: Path) -> list[tuple[set[int], set[int]]]:
     out = []
     lines = [x.rsplit(":")[1] for x in open(fn).readlines()]
     for line in lines:
         a, b = line.split("|")
-        out.append(([int(x) for x in a.split()], [int(x) for x in b.split()]))
+        out.append((set(int(x) for x in a.split()), set(int(x) for x in b.split())))
 
     return out
 
 
-def solve(input: list[tuple[list[int], list[int]]]) -> tuple[int, int]:
-    ans1 = 0
-    for lottery, mynumbers in input:
-        n_overlap = len(set(lottery).intersection(mynumbers))
-        ans1 += int(2 ** (n_overlap - 1))
-    ans2 = 0
+def solve(input: list[tuple[set[int], set[int]]]) -> tuple[int, int]:
+    ans1, quantity = 0, [1] * len(input)
+
+    for i, (lottery, mynumbers) in enumerate(input):
+        ans1 += int(2 ** (n_overlap := len(lottery & mynumbers) - 1))
+
+        for j in range(n_overlap):
+            quantity[i + j + 1] += quantity[i]
+
+    ans2 = sum(quantity)
     return ans1, ans2
 
 
