75b9ea9a6c8552f503a7226e39384ec2ad342260
Jaime Liew <jaimeliew1@gmail.com>
2023-12-10 21:25:25 -0500

    add day10 part 1

---
+++ advent_of_code_2023/day10.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+
+INPUT_FN = Path(__file__).parent.parent / "input/day10.txt"
+deltas = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+pipes: dict[str, list] = {
+    "|": [(1, 0), (-1, 0)],
+    "-": [(0, 1), (0, -1)],
+    "L": [(-1, 0), (0, 1)],
+    "J": [(0, -1), (-1, 0)],
+    "7": [(0, -1), (1, 0)],
+    "F": [(1, 0), (0, 1)],
+}
+
+
+def parse(fn: Path) -> list[str]:
+    return [line.strip() for line in open(fn).readlines()]
+
+
+def solve(input: list[str]) -> tuple[int, int]:
+    start = next(
+        ((i, j) for i, line in enumerate(input) if (j := line.find("S")) != -1)
+    )
+    direction = next(
+        (dx, dy)
+        for dx, dy in deltas
+        if (-dx, -dy) in pipes[input[start[0] + dx][start[1] + dy]]
+    )
+
+    visited = {start: direction}
+    current = (start[0] + direction[0], start[1] + direction[1])
+    while (this_pipe := input[current[0]][current[1]]) != "S":
+        direction = next(
+            (dx, dy)
+            for dx, dy in pipes[this_pipe]
+            if (dx, dy) != (-direction[0], -direction[1])
+        )
+        visited[current] = direction
+        current = (current[0] + direction[0], current[1] + direction[1])
+
+    ans1 = len(visited) // 2
+    ans2 = 0
+
+    return ans1, ans2
+
+
+if __name__ == "__main__":
+    print(solve(parse(INPUT_FN)))
