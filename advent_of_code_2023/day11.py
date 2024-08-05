3184e619e42a5e5b6a7b1365bd1e56b314dd5f74
Jaime Liew <jaimeliew1@gmail.com>
2024-08-05 17:21:49 +0200

    add day11 part 2

--- advent_of_code_2023/day11.py
+++ advent_of_code_2023/day11.py
@@ -8,29 +8,35 @@ def parse(fn: Path) -> list[str]:
     return [line.strip() for line in open(fn).readlines()]
 
 
-def expand(input):
-    out = []
-    for row in input:
-        out.append(row)
+def empty_indices(input):
+    indices = []
+    for i, row in enumerate(input):
         if all(x == "." for x in row):
-            out.append(row)
+            indices.append(i)
 
-    return out
+    return indices
 
 
 def solve(input: list[str]) -> tuple[int, int]:
-    expanded = expand(zip(*expand(input)))
+    empty_rows = empty_indices(input)
+    empty_cols = empty_indices(zip(*input))
+
     galaxies = []
-    for i, row in enumerate(expanded):
+    for i, row in enumerate(input):
         for j, col in enumerate(row):
             if col == "#":
                 galaxies.append((i, j))
 
-    ans1 = 0
+    ans1, ans2 = 0, 0
     for (x1, y1), (x2, y2) in combinations(galaxies, r=2):
-        ans1 += abs(x1 - x2) + abs(y1 - y2)
+        expanded = sum(
+            [(coord >= min(y1, y2)) and (coord < max(y1, y2)) for coord in empty_cols]
+        ) + sum(
+            [(coord >= min(x1, x2)) and (coord < max(x1, x2)) for coord in empty_rows]
+        )
 
-    ans2 = 0
+        ans1 += abs(x1 - x2) + abs(y1 - y2) + expanded * (2 - 1)
+        ans2 += abs(x1 - x2) + abs(y1 - y2) + expanded * (1000000 - 1)
 
     return ans1, ans2
 
